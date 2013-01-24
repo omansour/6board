b99dc735e52f42fb61a0e94556c08b389a96d58b
Tristan Bessoussa <tristan.bessoussa@gmail.com>
2013-01-24 16:25:19 +0100

    more logic

--- src/M6/Bundle/SixBoardBundle/Controller/SearchController.php
+++ src/M6/Bundle/SixBoardBundle/Controller/SearchController.php
@@ -8,8 +8,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Collections\ArrayCollection;
 
 use M6\Bundle\SixBoardBundle\Controller\Controller;
-use M6\Bundle\SixBoardBundle\Form\SearchType;
 use M6\Bundle\SixBoardBundle\Entity\Story;
+use M6\Bundle\SixBoardBundle\Form\SearchType;
+use M6\Bundle\SixBoardBundle\Form\SavedSearchType;
 
 /**
  * Search controller
@@ -70,6 +71,54 @@ class SearchController extends Controller
     public function resetAction(Request $request)
     {
         $filters = $this->setFilters('m6_search_board', array());
+
+        return $this->redirect($this->generateUrl('homepage'));
+    }
+
+    /**
+     * @Route("/filters/save", name="save_filters")
+     * @Template()
+     */
+    public function saveFilters(Request $request)
+    {
+        $form = $this->createForm(new SavedSearchType, $filters);
+
+        if ('POST' === $this->getRequest()->getMethod()) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+
+                $data = $form->getData();
+
+                $search = new Search;
+                $search->setSearch(serialize($this->getFilters('m6_search_board')));
+                $search->setPublic($data['public']);
+                $search->setName($data['name']);
+                $search->setUser($this->getUser());
+
+                $this->getDoctrine()->getEntityManager()->persist($search);
+                $this->getDoctrine()->getEntityManager()->flush();
+
+                $this->addFlash('success', sprintf('The search named %s has been successfully saved', $data['name']));
+            }
+        }
+
+        return array(
+            'form' => $form
+        );
+    }
+
+    /**
+     * @Route("/filters/load/{id}", name="load_saved_filter")
+     * @Template()
+     */
+    public function loadFilters(Search $search)
+    {
+        if ($search->isPublic() || $this->getUser()->getId() == $search->getUser()->getId()) {
+            $filters = $this->setFilters('m6_search_board', unserialize($search->getSearch()));
+        } else {
+            $this->addFlash('error', 'The search you\'re trying to access to is not public');
+        }
 
         return $this->redirect($this->generateUrl('homepage'));
     }
